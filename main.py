d01fc4cb3db9597cc4e2ce488ea1a5f218bd6e5a
Edouard Nicolas <edouard.n@proton.me>
2025-01-10 10:00:10 +0100

    allow all cors for staging

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from db.database import  create_db
 from api.main import api_router
 
@@ -7,6 +9,16 @@ create_db()
 
 # Création de l'application FastAPI
 app = FastAPI()
+
+#Allow all CORS for staging
+#todo: make all cors available only in staging
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Inclusion des routes de l'API
 app.include_router(api_router, prefix="/api/v1")
